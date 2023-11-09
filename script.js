b81ccb6c1f37ba72fbd1fa74591c1f8cb688a2d5
mateoh2467 <144353650+mateoh2467@users.noreply.github.com>
2023-11-09 15:14:44 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("askButton").addEventListener('click', function(){
+let answer = '';
+let randomIndex=Math.floor(Math.random()*5)
+if(randomIndex === 0){
+    answer = "it is certain";
+} else if (randomIndex === 1){
+    answer = "Ask again later";
+} else if (randomIndex === 2){
+    answer = "Yes, definetely";
+}
+});
