9c26b22d254ba8cfd0aa9c5373e264912a45db7e
mateoh2467 <144353650+mateoh2467@users.noreply.github.com>
2023-11-14 14:58:15 +0000

    made it work

--- script.js
+++ script.js
@@ -7,5 +7,21 @@ if(randomIndex === 0){
     answer = "Ask again later";
 } else if (randomIndex === 2){
     answer = "Yes, definetely";
+} else if (randomIndex === 3){
+    answer = "My reply is no";
+} else if (randomIndex === 4){
+    answer = "I cannot predict now";
+} else {
+    answer = "very doubtful";
+} 
+
+//Check if a questino is asked, trim method 
+let question = document.getElementById('question').value;
+if (question.trim() === ''){
+    alert("Please enter a question");
+} else {
+    document.getElementById('answer').textContent = answer
 }
+
 });
+
